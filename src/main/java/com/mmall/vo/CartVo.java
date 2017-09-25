a458d77dab62513094fdc9f733e4d00f399a0481
yong-yan <yong.yan@hpe.com>
2017-09-25 16:39:55 +0800

    section 8 update

---
+++ src/main/java/com/mmall/vo/CartVo.java
@@ -0,0 +1,44 @@
+package com.mmall.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CartVo {
+	private List<CartProductVo> cartProductVoList;
+	private BigDecimal cartTotalPrice;
+	private Boolean allchecked;
+	private String imageHost;
+
+	public List<CartProductVo> getCartProductVoList() {
+		return cartProductVoList;
+	}
+
+	public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
+		this.cartProductVoList = cartProductVoList;
+	}
+
+	public BigDecimal getCartTotalPrice() {
+		return cartTotalPrice;
+	}
+
+	public void setCartTotalPrice(BigDecimal cartTotalPrice) {
+		this.cartTotalPrice = cartTotalPrice;
+	}
+
+	public Boolean getAllchecked() {
+		return allchecked;
+	}
+
+	public void setAllchecked(Boolean allchecked) {
+		this.allchecked = allchecked;
+	}
+
+	public String getImageHost() {
+		return imageHost;
+	}
+
+	public void setImageHost(String imageHost) {
+		this.imageHost = imageHost;
+	}
+
+}
