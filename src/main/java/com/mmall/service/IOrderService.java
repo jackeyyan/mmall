68eb6bc14d26094166f696687a91969529f28411
yong-yan <yong.yan@hpe.com>
2017-11-08 19:52:48 +0800

    # WARNING: head commit changed in the meantime
    ""

---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,14 @@
+package com.mmall.service;
+
+import java.util.Map;
+
+import com.mmall.common.ServerResponse;
+
+public interface IOrderService {
+	
+	ServerResponse pay(Long orderNo,Integer userId, String path);
+	ServerResponse aliCallback(Map<String, String> params);
+	ServerResponse queryOrderPayStatus(Integer userId, Long orderNo);
+	ServerResponse createOrder(Integer userId, Integer shippingId);
+
+}
